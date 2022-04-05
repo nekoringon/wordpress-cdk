45dfebb9eeaa75c29ffa46b760f31baee7c16dc0
nekoringon <r.sato.shoot@gmail.com>
2022-04-05 01:06:55 +0000

    add subnet

--- wordpress-cdk/lib/wordpress-cdk-stack.ts
+++ wordpress-cdk/lib/wordpress-cdk-stack.ts
@@ -1,16 +1,37 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { CfnVPC } from "aws-cdk-lib/aws-ec2";
+import { CfnVPC, CfnSubnet } from "aws-cdk-lib/aws-ec2";
 
 export class WordpressCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
     
-    new CfnVPC(this, 'Vpc', {
-	cidrBlock: "172.16.0.0/17",
+    const vpc = new CfnVPC(this, 'Vpc', {
+	cidrBlock: "172.16.0.0/16",
 	enableDnsHostnames: true,
 	enableDnsSupport: true,
 	tags: [{ key: 'Name', value: 'wordpress-cdk-dev-vpc'}]
     });
+
+    const subnetPublic = new CfnSubnet(this, 'SubnetPublic', {
+	cidrBlock: "172.16.11.0/24",
+	vpcId: vpc.ref,
+	availabilityZone: 'ap-northeast-1a',
+	tags: [{ key: 'Name', value: 'wordpress-cdk-dev-subnet-public'}]
+    })
+    
+    const subnetApp = new CfnSubnet(this, 'SubnetApp', {
+	cidrBlock: "172.16.21.0/24",
+	vpcId: vpc.ref,
+	availabilityZone: 'ap-northeast-1a',
+	tags: [{ key: 'Name', value: 'wordpress-cdk-dev-subnet-app'}]
+    })
+
+    const subnetDb = new CfnSubnet(this, 'SubnetDb', {
+	cidrBlock: "172.16.22.0/24",
+	vpcId: vpc.ref,
+	availabilityZone: 'ap-northeast-1a',
+	tags: [{ key: 'Name', value: 'wordpress-cdk-dev-subnet-db'}]
+    })
   }
 }
